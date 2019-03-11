8ef6c524382dff5a29efff576b4d1643309ea354
lcagustini <torresmon235@gmail.com>
2019-03-11 10:58:14 -0300

    jsons

--- bot.js
+++ bot.js
@@ -1,18 +1,39 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+//
+var fs = require('fs');
 
 const { ActivityTypes } = require('botbuilder');
 
 class MyBot {
-    /**
-     *
-     * @param {TurnContext} on turn context object.
-     */
+    constructor(userState) {
+        this.realJSON = JSON.parse(fs.readFileSync('surrealista.json', 'utf8'))
+        this.fakeJSON = JSON.parse(fs.readFileSync('sensacionalista.json', 'utf8'))
+    }
+
     async onTurn(turnContext) {
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         if (turnContext.activity.type === ActivityTypes.Message) {
-            await turnContext.sendActivity(`You said '${ turnContext.activity.text }'`);
-        } else {
+            await turnContext.sendActivity(`${ this.realJSON[0].title }`);
+
+            if (turnContext.activity.text === "inicio") {
+            }
+        }
+        else if (turnContext.activity.type === ActivityTypes.ConversationUpdate) {
+            const activity = turnContext.activity;
+            if (activity.membersAdded) {
+                // Iterate over all new members added to the conversation.
+                for (const idx in activity.membersAdded) {
+                    if (activity.membersAdded[idx].id !== activity.recipient.id) {
+                        const welcomeMessage = `Bem Vindo ${ activity.membersAdded[idx].name }! ` +
+                            `Esse bot vai mandar noticias aleatorias, elas sao verdadeiras ou falsas?`;
+                        await turnContext.sendActivity(welcomeMessage);
+                        await this.sendSuggestedActions(turnContext);
+                    }
+                }
+            }
+        }
+        else {
             await turnContext.sendActivity(`[${ turnContext.activity.type } event detected]`);
         }
     }
